12b764eeacc5c42ea8387f5338aad487c6ff7c39
Avi Rulevich <avi8605@gmail.com>
2019-09-05 18:03:39 +0300

    Improve create api so it returns error if email exists

--- api/account/create.js
+++ api/account/create.js
@@ -3,6 +3,16 @@ const Account = require('../../models/account/Account');
 module.exports = async function(req, res, next) {
 	const {email, name, age} = req.body;
 	const account = new Account({email, name, age});
+	try{
 	await account.save();
+
+	}catch(err){
+
+			if (err.name === 'MongoError' && err.code === 11000) {
+			  return res.send({ error: 'email already exists' });
+			}
+		
+		res.send({message: err})
+	}
 	return res.send({message: 'success'});
 };
